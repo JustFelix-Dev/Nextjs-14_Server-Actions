fc7256b774082c67e38a19385cde6dd046baa5bd
Felix <owolabifelix78@gmail.com>
2024-04-10 09:15:53 +0100

    nextjs

--- components/main-header.js
+++ components/main-header.js
@@ -14,6 +14,7 @@ const MainHeader = () => {
   return (
     <>
         <MainBackground/>
+        
        <header className={classes.header}>
         <Link className={classes.logo} href='/'>
             <Image src={logoImg} alt="A plate with food on it" priority />
