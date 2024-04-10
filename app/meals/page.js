8f37c369774ed159b158335d63dd247315b2dead
Felix <owolabifelix78@gmail.com>
2024-04-10 10:12:13 +0100

    setting up sqlite3 db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "./meals-grid";
 
-const MealsPage = () => {
+const MealsPage = async() => {
   return (
-    <div>
-      MealsPage
-    </div>
+     <>
+     <header className={classes.header}>
+        <h1>Delicious meals, created <span className={classes.highlight} >by you</span></h1>
+        <p>Cook your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+            <Link href="/meals/share" > Share Your Favorite Recipe</Link>
+        </p>
+     </header>
+     <main className={classes.main}>
+     <MealsGrid meals={[]}/>
+     </main>
+     </>
   )
 }
 
