5067523a906f7c456e161644a52e3d8653384ef3
Felix <owolabifelix78@gmail.com>
2024-04-10 12:30:24 +0100

    dynamic routes and parameters

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import classes from "./page.module.css";
 import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
 
 const MealDetailPage = ({ params }) => {
     const slug = params.mealSlug;
+    const meal = getMeal(slug);
+    
+    if(!meal){
+        notFound()
+    }
+    meal.instructions =  meal.instructions.replace(/\n/g,'<br/>')
   return (
       <>
+      <div style={{display:'flex',alignItems:'center'}} >
       <header className={classes.header} >
            <div className={classes.image}>
-            <Image fill />
+            <Image  src={meal.image} alt={meal.title} fill />
            </div>
       </header>
-      <main></main>
+      <div className={classes.headerText} >
+        <h1>{meal.title}</h1>  
+        <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+        </p>
+        <p className={classes.summary}>{meal.summary}</p>
+      </div>
+      </div>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{__html:meal.instructions}} >
+
+        </p>
+      </main>
       </>
   )
 }
---
+++ app/meals/not-found.js
@@ -0,0 +1,11 @@
+
+const notFound= () => {
+  return (
+    <div className='not-found'>
+       <h1>Meal not Found!</h1>
+       <p>UNfortunately, meal not found!</p>
+    </div>
+  )
+}
+
+export default notFound
